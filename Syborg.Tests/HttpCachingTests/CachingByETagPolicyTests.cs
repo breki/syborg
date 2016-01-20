bb5d1a9a4789e091b04a8507f007414e99644a76
igor <226863+breki@users.noreply.github.com>
2016-01-20 07:00:43 +0100

    Reorganizing ContentCommand and caching policies code.

---
+++ Syborg.Tests/HttpCachingTests/CachingByETagPolicyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Syborg.Caching;
+
+namespace Syborg.Tests.HttpCachingTests
+{
+    public class CachingByETagPolicyTests : HttpCachingTestsBase
+    {
+        [Test]
+        public void CacheByETagShouldCloseResponseEvenWhenFresh ()
+        {
+            TimeSpan maxAge = TimeSpan.FromHours (1);
+            DateTime lastModifiedTime = new DateTime (2014, 06, 15, 18, 14, 00);
+            const string ETag = "x";
+            CachingByETagPolicy policy = new CachingByETagPolicy(maxAge, () => new Tuple<string, DateTime?>(ETag, lastModifiedTime));
+
+            DateTime cachedTime = lastModifiedTime;
+            WebContext.RequestHeaders.Add (HttpConsts.HeaderIfNoneMatch, ETag);
+
+            policy.ProcessRequest (null, WebContext, ReturnResource);
+
+            Assert.IsTrue (WebContext.IsResponseClosed);
+        }
+    }
+}
