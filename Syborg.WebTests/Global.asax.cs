3592f44245e06ba301e9a8f1d07f16acf728ff43
igor <igor@localhost>
2016-01-22 07:53:25 +0100

    Added more configuration to Syborg.WebTests.

--- Syborg.WebTests/Global.asax.cs
+++ Syborg.WebTests/Global.asax.cs
@@ -1,26 +1,56 @@
 ﻿using System;
+using System.Reflection;
 using System.Web;
-using System.Web.Routing;
+using log4net;
+using Syborg.Hosting;
 
 namespace Syborg.WebTests
 {
     public class Global : HttpApplication
     {
-        void Application_Start (object sender, EventArgs e)
+        protected void Application_Start (object sender, EventArgs e)
         {
-            // Code that runs on application startup
+            log4net.Config.XmlConfigurator.Configure ();
+
+            if (log.IsDebugEnabled)
+                log.Debug ("Application_Start");
+
+            appHost = new SyborgTestHttpModuleAppHost();
+            Application[SyborgHttpModule.KeyAppHost] = appHost;
         }
 
-        void Application_End (object sender, EventArgs e)
+        protected void Session_Start (object sender, EventArgs e)
         {
-            //  Code that runs on application shutdown
-
         }
 
-        void Application_Error (object sender, EventArgs e)
+        protected void Application_BeginRequest (object sender, EventArgs e)
         {
-            // Code that runs when an unhandled error occurs
-
         }
+
+        protected void Application_AuthenticateRequest (object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error (object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError ();
+            log.ErrorFormat ("Application_Error: {0}", ex);
+        }
+
+        protected void Session_End (object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End (object sender, EventArgs e)
+        {
+            if (log.IsDebugEnabled)
+                log.Debug ("Application_End");
+
+            if (appHost != null)
+                appHost.Dispose ();
+        }
+
+        private SyborgTestHttpModuleAppHost appHost;
+        private static readonly ILog log = LogManager.GetLogger (MethodBase.GetCurrentMethod ().DeclaringType);
     }
 }
---
+++ Syborg.WebTests/SyborgTestHttpModuleAppHost.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using LibroLib.FileSystem;
+using LibroLib.Misc;
+using Syborg.Commands;
+using Syborg.ContentHandling;
+using Syborg.Hosting;
+using Syborg.Razor;
+using Syborg.Routing;
+
+namespace Syborg.WebTests
+{
+    public class SyborgTestHttpModuleAppHost : SyborgHttpModuleAppHost
+    {
+        public SyborgTestHttpModuleAppHost()
+        {
+            IFileSystem fileSystem = new WindowsFileSystem ();
+            IApplicationInfo applicationInfo = new ApplicationInfo ();
+            ITimeService timeService = new RealTimeService ();
+            IRazorCompiler razorCompiler = new InMemoryRazorCompiler ();
+            IRazorViewRenderingEngine viewRenderingEngine = new RazorViewRenderingEngine (fileSystem, razorCompiler);
+
+            IWebServerConfiguration config = new WebServerConfiguration();
+
+            FileMimeTypesMap fileMimeTypesMap = new FileMimeTypesMap ().RegisterStandardMimeTypes ();
+
+            Initialize (config, fileSystem, applicationInfo, timeService, fileMimeTypesMap, viewRenderingEngine);
+
+            IFileCache fileCache = new FileCache ();
+
+            string webAppRootDir;
+
+            if (!WebServerConfiguration.WebServerDevelopmentMode)
+            {
+#if NCRUNCH
+                                webAppRootDir = @"D:\hg\ScalableMaps\WebApp\ScalableMaps\ScalableMaps.Web2";
+#else
+                webAppRootDir = ApplicationInfo.GetAppDirectoryPath ("..");
+#endif
+            }
+            else
+                webAppRootDir = ApplicationInfo.GetAppDirectoryPath ("..");
+
+            ContentCommand contentCommand = RegisterWebContent (webAppRootDir, fileCache, config);
+
+            AddRoute (new RegexWebRequestRoute ("^Content/(?<path>.+)$", HttpMethod.GET, contentCommand));            
+        }
+
+        private ContentCommand RegisterWebContent (string webAppRootDir, IFileCache fileCache, IWebServerConfiguration config)
+        {
+            string contentRootDirectory = Path.GetFullPath (Path.Combine (webAppRootDir, "Content"));
+
+            ContentCommand contentCommand = new ContentCommand (contentRootDirectory, FileSystem, fileCache);
+            contentCommand.CacheByMaxAge (@"cached-by-max-age.txt", TimeSpan.FromDays (30));
+            contentCommand.CacheByETag (@"cached-by-etag.txt", TimeSpan.FromDays(10), () => GenerateContentETag (config));
+            return contentCommand;
+        }
+
+        private static Tuple<string, DateTime?> GenerateContentETag (IWebServerConfiguration config)
+        {
+            return new Tuple<string, DateTime?> ("\"1234\"", DateTime.Now);
+        }
+    }
+}
