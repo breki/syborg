028e12de9cf78b1d2471a263ec52be1dbe07b478
igor <226863+breki@users.noreply.github.com>
2016-01-22 07:37:11 +0100

    Added Syborg.WebTests project.

---
+++ Syborg.WebTests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Syborg.WebTests.Global" Language="C#" %>
--- Syborg/Hosting/SyborgHttpModule.cs
+++ Syborg/Hosting/SyborgHttpModule.cs
@@ -1,5 +1,4 @@
 using System;
-using System.CodeDom;
 using System.Globalization;
 using System.Net;
 using System.Reflection;
