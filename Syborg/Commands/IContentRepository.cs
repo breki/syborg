2bdc8ea9fed99561122ada5bbecaa31de911c873
igor <226863+breki@users.noreply.github.com>
2016-01-25 09:16:36 +0100

    Some code contracts additions.

--- Syborg/Commands/IContentRepository.cs
+++ Syborg/Commands/IContentRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace Syborg.Commands
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Syborg.Commands
 {
     /// <summary>
     /// Provides methods to access the content files.
     /// </summary>
+    [ContractClass(typeof(IContentRepositoryContract))]
     public interface IContentRepository
     {
         /// <summary>
@@ -11,5 +15,16 @@
         /// <param name="contentFilePath">The path of the content file.</param>
         /// <returns><c>true</c> if the file exists; <c>false</c> otherwise</returns>
         bool DoesFileExist(string contentFilePath);
+    }
+
+    [ContractClassFor(typeof(IContentRepository))]
+    // ReSharper disable once InconsistentNaming
+    internal abstract class IContentRepositoryContract : IContentRepository
+    {
+        bool IContentRepository.DoesFileExist(string contentFilePath)
+        {
+            Contract.Requires(contentFilePath != null);
+            throw new InvalidOperationException();
+        }
     }
 }
