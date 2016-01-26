98e9a46c1cab3f549d94e3e29d8d164526956a8a
igor <igor@localhost>
2016-01-26 12:53:43 +0100

    Fiddling with SimpleTestHttpModule to try to solve the chunked mistery, to no avail.

--- Syborg.WebTests/SimpleTestHttpModule.cs
+++ Syborg.WebTests/SimpleTestHttpModule.cs
@@ -26,71 +26,82 @@ namespace Syborg.WebTests
             HttpApplication application = (HttpApplication)sender;
             HttpContext httpContext = application.Context;
 
-            string text = @"The cave bear (Ursus spelaeus) was a species of bear that lived in Europe during the Pleistocene and became extinct about 24,000 years ago during the Last Glacial Maximum.
-Both the name 'cave' and the scientific name spelaeus are because fossils of this species were mostly found in caves, showing that cave bears may have spent more time in caves than the brown bear, which uses caves only for hibernation. Consequently, in the course of time, whole layers of bones, almost entire skeletons, were found in many caves.";
+            string text = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
             byte[] uncompressedBytes = new UTF8Encoding (false).GetBytes (text);
 
-            httpContext.Response.ContentType = HttpConsts.ContentTypeTextPlain;
+            httpContext.Response.ClearHeaders();
+            httpContext.Response.ContentType = "text/plain";
 
+            //WriteUncompressed(httpContext, uncompressedBytes);
             GZipCompressAndWrite(httpContext, uncompressedBytes);
             //DeflateCompressAndWrite(httpContext, uncompressedBytes);
 
-            httpContext.Response.End();
+            //httpContext.Response.End();
+            HttpContext.Current.ApplicationInstance.CompleteRequest();
+        }
+
+        private static void WriteUncompressed(HttpContext context, byte[] bytes)
+        {
+            string contentLenStr = bytes.Length.ToString (CultureInfo.InvariantCulture);
+            HttpContext.Current.Response.Headers["Content-Length"] = contentLenStr;
+            context.Response.Headers["Content-Length"] = contentLenStr;
+            context.Response.BinaryWrite (bytes);
         }
 
         private static void GZipCompressAndWrite (HttpContext context, byte[] bytes)
         {
-            context.Response.AddHeader (HttpConsts.HeaderContentEncoding, "gzip");
-
             byte[] compressed = GZipCompressByteArray (bytes);
             log.DebugFormat ("Compressed {0} bytes to {1}", bytes.Length, compressed.Length);
 
-            HttpContext.Current.Response.Headers[HttpConsts.HeaderContentLength]
-                = compressed.Length.ToString (CultureInfo.InvariantCulture);
-            context.Response.BinaryWrite (compressed);
+            string contentLenStr = compressed.Length.ToString (CultureInfo.InvariantCulture);
+            HttpContext.Current.Response.Headers["Content-Length"] = contentLenStr;
+            context.Response.Headers["Content-Length"] = contentLenStr;
+            context.Response.AddHeader ("Content-Encoding", "gzip");
+
+            using (GZipStream compressStream = new GZipStream (context.Response.OutputStream, CompressionMode.Compress, true))
+                compressStream.Write (bytes, 0, bytes.Length);
+
+            //context.Response.BinaryWrite (compressed);
         }
 
-        private static void DeflateCompressAndWrite (HttpContext context, byte[] bytes)
-        {
-            context.Response.AddHeader (HttpConsts.HeaderContentEncoding, "deflate");
+        //private static void DeflateCompressAndWrite (HttpContext context, byte[] bytes)
+        //{
+        //    context.Response.AddHeader (HttpConsts.HeaderContentEncoding, "deflate");
             
-            byte[] compressed = DeflateCompressByteArray(bytes);
-            log.DebugFormat("Compressed {0} bytes to {1}", bytes.Length, compressed.Length);
+        //    byte[] compressed = DeflateCompressByteArray(bytes);
+        //    log.DebugFormat("Compressed {0} bytes to {1}", bytes.Length, compressed.Length);
 
-            HttpContext.Current.Response.Headers[HttpConsts.HeaderContentLength]
-                = compressed.Length.ToString (CultureInfo.InvariantCulture);
-            context.Response.BinaryWrite (compressed);
-        }
+        //    HttpContext.Current.Response.Headers[HttpConsts.HeaderContentLength]
+        //        = compressed.Length.ToString (CultureInfo.InvariantCulture);
+        //    context.Response.BinaryWrite (compressed);
+        //}
 
         private static byte[] GZipCompressByteArray (byte[] data)
         {
             using (MemoryStream outputStream = new MemoryStream ())
             {
                 using (GZipStream compressStream = new GZipStream(outputStream, CompressionMode.Compress, false))
-                {
                     compressStream.Write (data, 0, data.Length);
-                    compressStream.Close();
-                }
 
                 outputStream.Flush();
                 return outputStream.ToArray ();
             }
         }
 
-        private static byte[] DeflateCompressByteArray (byte[] data)
-        {
-            using (MemoryStream outputStream = new MemoryStream ())
-            {
-                using (DeflateStream compressStream = new DeflateStream (outputStream, CompressionMode.Compress, false))
-                {
-                    compressStream.Write (data, 0, data.Length);
-                    compressStream.Close();
-                }
+        //private static byte[] DeflateCompressByteArray (byte[] data)
+        //{
+        //    using (MemoryStream outputStream = new MemoryStream ())
+        //    {
+        //        using (DeflateStream compressStream = new DeflateStream (outputStream, CompressionMode.Compress, false))
+        //        {
+        //            compressStream.Write (data, 0, data.Length);
+        //            compressStream.Close();
+        //        }
 
-                outputStream.Flush();
-                return outputStream.ToArray ();
-            }
-        }
+        //        outputStream.Flush();
+        //        return outputStream.ToArray ();
+        //    }
+        //}
 
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
     }
