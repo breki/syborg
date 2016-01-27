48e1a763db02d2e03b10cb2563ae6f36b1f7ccc4
igor <226863+breki@users.noreply.github.com>
2016-01-27 08:46:37 +0100

    Writing AppVeyor install script.

---
+++ AppVeyor/BuildInstall.ps1
@@ -0,0 +1,18 @@
+Install-WindowsFeature Web-WebServer -IncludeAllSubFeature
+
+Import-Module WebAdministration
+$iisAppPoolName = "syborg"
+$iisAppPoolDotNetVersion = "v4.0"
+$iisAppName = "syborg-tests"
+$directoryPath = "Syborg.WebTests"
+
+#navigate to the app pools root
+cd IIS:\AppPools\
+
+#check if the app pool exists
+if (!(Test-Path $iisAppPoolName -pathType container))
+{
+    #create the app pool
+    $appPool = New-Item $iisAppPoolName
+    $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+}
