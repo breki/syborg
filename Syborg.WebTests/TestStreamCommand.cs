4af359a2ac28cfbfd0bfe5a6d7b50a8dd61f48b7
igor <226863+breki@users.noreply.github.com>
2016-01-27 12:13:03 +0100

    More StreamResultIntegrationTests.

--- Syborg.WebTests/TestStreamCommand.cs
+++ Syborg.WebTests/TestStreamCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
+using System.Net;
 using System.Text;
+using LibroLib;
 using Syborg.CommandResults;
 using Syborg.Routing;
 
@@ -9,10 +11,39 @@ namespace Syborg.WebTests
     {
         public IWebCommandResult Execute(IWebContext context, WebRequestRouteMatch routeMatch)
         {
-            const string Text = "This is a stream response";
-            MemoryStream stream = new MemoryStream(new UTF8Encoding(false).GetBytes(Text));
+            string path = routeMatch["path"];
 
-            return new StreamResult(stream, HttpConsts.ContentTypeTextPlain);
+            if (path == null)
+                return new HttpStatusResult (HttpStatusCode.BadRequest);
+
+            switch (path)
+            {
+                case "short-text":
+                    return ReturnShortText();
+                case "long-binary":
+                    return ReturnLongBinary();
+                default:
+                    return new HttpStatusResult(HttpStatusCode.BadRequest);
+            }
+        }
+
+        private static IWebCommandResult ReturnShortText()
+        {
+            const string Text = "This is a stream response";
+            MemoryStream stream = new MemoryStream (new UTF8Encoding (false).GetBytes (Text));
+
+            return new StreamResult (stream, HttpConsts.ContentTypeTextPlain);
+        }
+
+        private static IWebCommandResult ReturnLongBinary()
+        {
+            const int StreamLength = 4 * 1024 * 1024;
+            byte[] data = new byte[StreamLength];
+            for (int i = 0; i < StreamLength; i++)
+                data[i] = (byte)(i % 256);
+
+            MemoryStream stream = new MemoryStream (data);
+            return new StreamResult (stream, HttpConsts.ContentTypeApplicationPdf);
         }
     }
 }
