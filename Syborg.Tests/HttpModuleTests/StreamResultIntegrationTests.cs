bc92cc0e904552d62f12633738554eb71a861715
igor <igor@localhost>
2016-01-27 12:13:03 +0100

    More StreamResultIntegrationTests.

--- Syborg.Tests/HttpModuleTests/StreamResultIntegrationTests.cs
+++ Syborg.Tests/HttpModuleTests/StreamResultIntegrationTests.cs
@@ -7,13 +7,25 @@ namespace Syborg.Tests.HttpModuleTests
     public class StreamResultIntegrationTests : HttpModuleTestsBase
     {
         [Test]
-        public void Test()
+        public void CheckShortTextResponse()
         {
             using (IRestClient client = RestClientFactory.CreateRestClient ())
             {
-                IRestClientResponse response = client.Get (WebAppUrl + "stream/stream-result").Do ().Response;
+                IRestClientResponse response = client.Get (WebAppUrl + "stream/short-text").Do ().Response;
                 Assert.AreEqual ((int)HttpStatusCode.OK, client.StatusCode);
                 Assert.AreEqual("This is a stream response", response.AsString());
+            }
+        }
+
+        [Test]
+        public void CheckLongBinaryResponse()
+        {
+            using (IRestClient client = RestClientFactory.CreateRestClient ())
+            {
+                IRestClientResponse response = client.Get (WebAppUrl + "stream/long-binary").Do ().Response;
+                Assert.AreEqual ((int)HttpStatusCode.OK, client.StatusCode);
+                byte[] bytes = response.AsBytes();
+                Assert.AreEqual(4 * 1024 * 1024, bytes.Length);
             }
         }
     }
